a6b40eb131f4835f39a70bd7628d0184dbdfb67a
Michiel Meeuwissen <michiel meeuwissen>
2025-01-06 09:05:35 +0100

    Fixed module name

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,14 @@
 /**
  * See {@link Mapper}
  */
-module org.meeuw.mapping.annotations {
+module nl.beeldengeluid.mapping.annotations {
     requires static lombok;
-    requires transitive org.slf4j;
 
+    requires transitive org.slf4j;
     requires transitive com.fasterxml.jackson.databind;
-    requires json.path;
     requires transitive jakarta.xml.bind;
+
+    requires json.path;
     requires org.meeuw.functional;
 
     exports nl.beeldengeluid.mapping.annotations;
