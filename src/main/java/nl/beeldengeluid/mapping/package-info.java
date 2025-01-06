93277e3af82edb586875cc796cc9172b7f535073
Michiel Meeuwissen <michiel meeuwissen>
2025-01-06 09:08:24 +0100

    javadoc.

--- src/main/java/nl/beeldengeluid/mapping/package-info.java
+++ src/main/java/nl/beeldengeluid/mapping/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Contains the {@link nl.beeldengeluid.mapping.Mapper} utility itself.
- * Relevant annotations(s) are in {@link org.meeuw.mapping.annotations}
- * @see <a href="https://github.com/mihxil/mapping-annotations">https://github.com/mihxil/mapping-annotations</a>
+ * Relevant annotations(s) are in {@link nl.beeldengeluid.mapping.annotations}
+ * @see <a href="https://github.com/beeldengeluid/mapping-annotations">https://github.com/beeldengeluid/mapping-annotations</a>
  */
 package nl.beeldengeluid.mapping;
